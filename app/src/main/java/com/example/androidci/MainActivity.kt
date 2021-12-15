5e78637e64bd84501404dafe5c2c0dcd9a593d06
GamalSebaee <gamal.sebaee@aboughalymotos.com>
2021-12-15 14:13:31 +0200

    update home message

--- app/src/main/java/com/example/androidci/MainActivity.kt
+++ app/src/main/java/com/example/androidci/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Gamal be3mel test for circle ci", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
